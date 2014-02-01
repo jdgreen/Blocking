9afea78208d7aafffa1bde1e481abaefab01e69f
J Green <jonathangreen07@gmail.com>
2014-02-01 12:19:47 +0000

    Update src.py

--- src/src.py
+++ src/src.py
@@ -4,18 +4,18 @@
 try:
 	import os
 	directoryListing = os.listdir("/media/")
-	if 'KINGSTON' in directoryListing:
-		king = '/media/KINGSTON/blocking/gen_data/'
-		king_gra = '/media/KINGSTON/blocking/graphs/'
-		king_dat = '/media/KINGSTON/blocking/data/pkl_files/blocking/'
-	elif 'jonathan' in directoryListing:
-		king = '/media/jonathan/KINGSTON/blocking/gen_data/'
-		king_gra = '/media/jonathan/KINGSTON/blocking/graphs/'
-		king_dat = '/media/jonathan/KINGSTON/blocking/data/pkl_files/blocking/'
-	elif 'jonny' in directoryListing:
-		king = '/media/jonny/KINGSTON/blocking/gen_data/'
-		king_gra = '/media/jonny/KINGSTON/blocking/graphs/'
-		king_dat = '/media/jonny/KINGSTON/blocking/data/pkl_files/blocking/'
+	if '[insert directory signifyer here]' in directoryListing:
+		king =
+		king_gra =
+		king_jet =
+		king_dat =
+		king_xfwnl =
+		king_xfwnh =
+		king_xhjjb =
+		king_xhjjc =
+		king_xhjjd =
+		king_sol =
+		king_list = 
 
 except IOError as err:	print "Cannot import module: " + str(err)
 
